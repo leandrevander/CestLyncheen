b5137a01d871e2a1da759d3f127d9320fadf5942
Thomas.T <t.triopon@rubika-edu.com>
2025-10-02 17:19:59 +0200

    Pathfinding

---
+++ Assets/IAZombie.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IAZombie : MonoBehaviour
+{
+    public GameObject player;
+    public NavMeshAgent agent;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            agent.destination = player.transform.position;
+        }
+        
+    }
+}
