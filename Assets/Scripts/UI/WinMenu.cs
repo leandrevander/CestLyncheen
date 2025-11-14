12077caa7349afdec101a4664483d08d1a4435be
leandrevander <l.vandercryssen@rubika-edu.com>
2025-11-14 17:44:06 +0100

    menu principal avec le choix des niveaux qui marche y'a juste l'ecran de mort qui marche pas et je comprends pourquoi mais la j'ai pas le temps de modifier donc azy on fera plus tard

--- Assets/Scripts/UI/WinMenu.cs
+++ Assets/Scripts/UI/WinMenu.cs
@@ -14,7 +14,6 @@ public class WinMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     { 
-        gamepad = Gamepad.current;
         winMenuUI.SetActive(false);
     }
 
