536c8e65f4fa0de6da9a9bdd0e9e6481bbfe288a
leandrevander <l.vandercryssen@rubika-edu.com>
2025-11-25 11:14:23 +0100

    prefab shadow bubble

--- Assets/Scripts/Light script/DetectorLight.cs
+++ Assets/Scripts/Light script/DetectorLight.cs
@@ -9,6 +9,7 @@ public class DetectorLight : MonoBehaviour
     private float                 zombieDistance;
     private float                 distance;
     private EnemyHealthManagement enemyHealth;
+    private ShadowBubble shadowBubble;
     public  LayerMask             raycastMask;
     private WeaponsManager        weaponsManager;
     public  Light2D               flashlightLight;
@@ -35,37 +36,68 @@ public class DetectorLight : MonoBehaviour
     {
         if (other.CompareTag("Ennemi"))
         {
-            
-            
+
+
             cibleZombie = other.transform;
             Vector2 direction = (cibleZombie.position - player.transform.position).normalized;
-            distance = Vector2.Distance(player.transform.position,cibleZombie.position);
-            
+            distance = Vector2.Distance(player.transform.position, cibleZombie.position);
+
             RaycastHit2D hit = Physics2D.Raycast(player.transform.position, direction, distance, raycastMask);
-            
+
             Debug.DrawRay(player.transform.position, direction * distance, Color.red);
-            
-            if (hit.collider !=null && hit.collider == other)
+
+            if (hit.collider != null && hit.collider == other)
             {
 
                 enemyHealth = cibleZombie.GetComponent<EnemyHealthManagement>();
 
                 Debug.Log("LE RAYCAST TOUCHE LE ZOMBIE DIEU MERCI");
                 enemyHealth.isHittenByFlashlight = true;
-                
-                
+
+
             }
             else
             {
-                enemyHealth                         = cibleZombie.GetComponent<EnemyHealthManagement>();
+                enemyHealth                      = cibleZombie.GetComponent<EnemyHealthManagement>();
                 enemyHealth.isHittenByFlashlight = false;
-                
+
             }
-               
-               
+
+
+
+        }
+
+        else if (other.CompareTag("ShadowBubble"))
+        {
+
+
+            cibleZombie = other.transform;
+            Vector2 direction = (cibleZombie.position - player.transform.position).normalized;
+            distance = Vector2.Distance(player.transform.position, cibleZombie.position);
+
+            RaycastHit2D hit = Physics2D.Raycast(player.transform.position, direction, distance, raycastMask);
+
+            Debug.DrawRay(player.transform.position, direction * distance, Color.red);
+
+            if (hit.collider != null && hit.collider == other)
+            {
+
+                shadowBubble = cibleZombie.GetComponent<ShadowBubble>();
+
+                Debug.Log("LE RAYCAST TOUCHE LA BUBBLE DIEU MERCI");
+                shadowBubble.isHittenByFlashlight = true;
+
+
+            }
+            else
+            {
+                shadowBubble                      = cibleZombie.GetComponent<ShadowBubble>();
+                shadowBubble.isHittenByFlashlight = false;
+
+            }
         }
     }
-    
+
 
     // Flashlight level 4 upgrade : Range
     public void FlashlightLevel3()
--- Assets/Scripts/Meta Data/ShadowBubble.cs
+++ Assets/Scripts/Meta Data/ShadowBubble.cs
@@ -8,6 +8,10 @@ public class ShadowBubble : MonoBehaviour
     public WeaponsManager weaponsManager;
     public bool           isHittenByFlashlight = false;
     public Coroutine      coroutine;
+    public GameObject     metaDataOrb;
+    public GameObject     healthGO;
+
+    int randomNumber;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,20 +22,28 @@ public class ShadowBubble : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (isHittenByFlashlight == true && coroutine == null)
+        {
+            coroutine = StartCoroutine(PerteDePv());
+        }
     }
 
     IEnumerator PerteDePv()
     {
-
-
         HealthZombie = HealthZombie - weaponsManager.hitByFlashlight;
         if (HealthZombie <= 0)
         {
             Destroy(gameObject);
-            //Instantiate(experiencepointPrefab, transform.position, transform.rotation);
+            randomNumber  = Random.Range(0, 2);
+            if (randomNumber == 1)
+            {
+                Instantiate(metaDataOrb, transform.position, transform.rotation);
+            }
+            else
+            {
+                Instantiate(healthGO, transform.position, transform.rotation);
+            }
         }
-
         yield return new WaitForSeconds(1);
         coroutine            = null;
         isHittenByFlashlight = false;
