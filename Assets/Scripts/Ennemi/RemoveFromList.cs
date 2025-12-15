9064cb6af856473cea2bf361be1a032ddb17d351
Thomas.T <t.triopon@rubika-edu.com>
2025-12-15 16:31:18 +0100

    Musique intro + radar ennemi (avec le son)

--- Assets/Scripts/Ennemi/RemoveFromList.cs
+++ Assets/Scripts/Ennemi/RemoveFromList.cs
@@ -5,8 +5,11 @@ public class RemoveFromList : MonoBehaviour
     public EnnemiesDetector detector;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
 
+    private void Start()
+    {
+        detector = GameObject.FindGameObjectWithTag("Detector").GetComponent<EnnemiesDetector>();
+    }
     // Update is called once per frame
     private void OnDestroy()
     {
