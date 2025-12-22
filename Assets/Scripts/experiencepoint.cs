47d3a9dd4cd5520c9a3bdd99604d7b965c9acfe1
gd/Thomas <t.triopon@rubika-edu.com>
2025-12-22 19:36:40 +0100

    Light weapons/ Exp sounds fix/Monster Detector Fix

--- Assets/Scripts/experiencepoint.cs
+++ Assets/Scripts/experiencepoint.cs
@@ -7,33 +7,37 @@ using TMPro;
 public class experiencepoint : MonoBehaviour
 {
     [SerializeField] AudioSource experiencepointSound;
-    public           Slider      BarreXp;
-    public           int         ExpReward = 3;
+    public Slider BarreXp;
+    public int ExpReward = 3;
 
     public delegate void EnnemiDefeated(int exp);
     public static event EnnemiDefeated OnEnnemiDefeated;
-    
-    
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+
         {
-            experiencepointSound.Play();
-            OnEnnemiDefeated(ExpReward);
-            Destroy(gameObject,0.1f);
+            if (other.gameObject.CompareTag("Player"))
+            {
+                AudioSource.PlayClipAtPoint(
+                    experiencepointSound.clip,
+                    transform.position
+                );
+
+                OnEnnemiDefeated(ExpReward);
+                Destroy(gameObject);
+            }
         }
 
 
-
     }
-
-    
 }
