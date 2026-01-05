1780197e11d5026512a947f9a8f9fad949667f7e
leandrevander <l.vandercryssen@rubika-edu.com>
2026-01-05 22:49:03 +0100

    Refonte main menu

--- Assets/Scripts/Meta Data/PlayerData.cs
+++ Assets/Scripts/Meta Data/PlayerData.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
-	public int metaData;
-	public int unlockedBulbDef;
-	public int unlockedCameraDef;
-	public int unlockedGlowstickDef;
-	public int unlockedLevel2Def;
+	public int   metaData;
+	public int   unlockedBulbDef;
+	public int   unlockedCameraDef;
+	public int   unlockedGlowstickDef;
+	public int   unlockedLevel2Def;
 	public float musicVol;
 	public float sfxVol;
+	public int   snailsKilled;
+	public int   seagullsKilled;
+	public int   sharksKilled;
+	public int   dolphinsKilled;
+
 
 	MetaDataSystem metaDataSystem;
 	
@@ -22,6 +27,10 @@ public class PlayerData : MonoBehaviour
 		unlockedLevel2Def    = metaDataSystem.CurrentUnlockedLevel2Def;
 		musicVol         = metaDataSystem.CurrentMusicVol;
 		sfxVol         = metaDataSystem.CurrentSFXVol;
+		snailsKilled      = metaDataSystem.CurrentSnailsKilled;
+		sharksKilled       = metaDataSystem.CurrentSharksKilled;
+		seagullsKilled       = metaDataSystem.CurrentSeagullsKilled;
+		dolphinsKilled = metaDataSystem.CurrentDolphinsKilled;
 
 
 	}
